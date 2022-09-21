1439a98c17a9c578d178b1a8bcc4a9c0542b5658
NjambaSoul <mainaelsie4@gmail.com>
2022-09-21 10:49:56 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
  * Return: returns pointer to @dest 
  */
 
- char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
          
 	int index = 0, dest_len = 0;
@@ -18,7 +18,7 @@
                 dest_len++;
 
         for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	        dest[dest_len++] = src[index];
 
         return (dest);
 }
