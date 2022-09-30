9dab7694d64c4c34c9e4d85f71c8c2fd9cf5e2e8
NjambaSoul <mainaelsie4@gmail.com>
2022-09-30 14:44:20 -0700

    Task 0 & Task 1

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
-*_memset - fills a memory block with a constant byte
-*@s: address to memory block
-*@b: char to be used
-*@n: number of bytes used
-*
-*Return: pointer to memory block
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-while (n)
-{
-s[n - 1] = b;
-n--;
-}
-return (s);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,25 +1,29 @@
 #include "main.h"
-
 /**
- * _strpbrk - searches a string for any of a set of bytes.
- * @s: the string to be searched
- * @accept: the set of bytes to be searched for
- * Return: if a set is matched - a pointer to the matched byte.
- * if no set is matched - NULL
- */
+*_strpbrk - The _strpbrk() function locates the first
+* occurrence in the string s of any of the bytes in
+*the string accept
+*
+*@s: string where search is made
+*@accept: string where searched bytes are located
+*
+*Return:Returns a pointer to the byte in s that matches
+* one of the bytes in accept, or NULL if no such byte is found
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
-	int index;
+	int i;
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept[index])
+			if (accept[i] == *s)
 				return (s);
 		}
 		s++;
 	}
+
 	return ('\0');
 }
