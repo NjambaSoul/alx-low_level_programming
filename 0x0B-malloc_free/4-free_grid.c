57b4332c25d766f2941684b846f8f0d53433c953
NjambaSoul <mainaelsie4@gmail.com>
2022-10-04 10:42:40 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,58 +1,17 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* len - returns length of str
-* @str: string counted
-* Return: returns the length
-*/
-int len(char *str)
-{
-int len = 0;
-
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
-}
-
-/**
-* argstostr - a function that concatenates all the arguments of your program
-* @ac: count for args passed to the function
-* @av: array of arguments
-*
-* Return: pointer to the new string
+* free_grid - free the grid
+* @grid: grid of memories
+* @height: int
+* Return: void
 */
 
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
-char *new_string = NULL;
-int k = 0, i = ac, j, sum = 0, temp = 0;
-
-if (ac == 0 || av == NULL)
-return (NULL);
-
-while (ac--)
-sum += (len(av[ac]) + 1);
-new_string = (char *) malloc(sum + 1);
-
-if (new_string != NULL)
-{
-while (k < i)
-{
-for (j = 0; av[k][j] != '\0'; j++)
-new_string[j + temp] = av[k][j];
-new_string[temp + j] = '\n';
-temp += (j + 1);
-k++;
+int i = 0;
+for (; i < height; i++)
+free(grid[i]);
+free(grid);
 }
-new_string[temp] = '\0';
-}
-else
-{
-return (NULL);
-}
-return (new_string);
-}
-
